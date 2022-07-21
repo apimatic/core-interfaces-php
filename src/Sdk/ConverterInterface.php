f6b1d5abfa0a0187bfcc62be47be2a9eac6ada59
Asad Ali <asad.ali@apimatic.io>
2022-07-21 09:31:58 +0500

    updates createApiResponse converterInterfaceMethod

--- src/Sdk/ConverterInterface.php
+++ src/Sdk/ConverterInterface.php
@@ -4,19 +4,14 @@ namespace CoreDesign\Sdk;
 
 use CoreDesign\Core\ContextInterface;
 use CoreDesign\Core\CoreExceptionInterface;
-use CoreDesign\Core\CoreResponseInterface;
 use CoreDesign\Core\RequestInterface;
 use CoreDesign\Core\ResponseInterface;
 
 interface ConverterInterface
 {
-    /**
-     * @param CoreExceptionInterface $exception
-     * @return mixed
-     */
     public function createApiException(CoreExceptionInterface $exception);
     public function createHttpContext(ContextInterface $context);
     public function createHttpRequest(RequestInterface $request);
     public function createHttpResponse(ResponseInterface $response);
-    public function createApiResponse(CoreResponseInterface $coreResponse);
+    public function createApiResponse(ContextInterface $context, $deserializedBody);
 }
