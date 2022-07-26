aac34125ecab9a09984525d75f254b125de2ac8a
Asad Ali <asad.ali@apimatic.io>
2022-07-26 17:58:53 +0500

    Updated Request setter interface

--- src/Core/Request/RequestSetterInterface.php
+++ src/Core/Request/RequestSetterInterface.php
@@ -2,6 +2,8 @@
 
 namespace CoreDesign\Core\Request;
 
+use CoreDesign\Core\BodyFormat;
+
 interface RequestSetterInterface extends RequestInterface
 {
     public function setHttpMethod(string $requestMethod): void;
@@ -10,6 +12,6 @@ interface RequestSetterInterface extends RequestInterface
     public function addTemplate(string $key, $value, bool $encode = true): void;
     public function addHeader(string $key, $value): void;
     public function addFormParam(string $key, $value, string $arrayFormat = RequestArraySerialization::INDEXED): void;
-    public function addBodyParam(string $key, $value, bool $wrapInObject = true): void;
+    public function addBodyParam(string $key, $value, bool $wrapObject = true, string $format = BodyFormat::JSON): void;
     public function setRetryOption(string $retryOption): void;
 }
