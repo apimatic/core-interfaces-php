8e8a41573c303bd44c5a703e1c05097da808ebe9
Asad Ali <asad.ali@apimatic.io>
2022-07-26 16:38:21 +0500

    Addition of new methods in requestSetterInterface

--- src/Core/Request/RequestSetterInterface.php
+++ src/Core/Request/RequestSetterInterface.php
@@ -4,6 +4,8 @@ namespace CoreDesign\Core\Request;
 
 interface RequestSetterInterface extends RequestInterface
 {
+    public function setHttpMethod(string $requestMethod): void;
+    public function appendPath(string $path): void;
     public function addQuery(string $key, $value, string $arrayFormat = RequestArraySerialization::INDEXED): void;
     public function addTemplate(string $key, $value, bool $encode = true): void;
     public function addHeader(string $key, $value): void;
