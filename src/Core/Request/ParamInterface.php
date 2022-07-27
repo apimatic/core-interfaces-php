44c1041e0ea1e4c34c4488e39b4be45198eeb9c7
Asad Ali <asad.ali@apimatic.io>
2022-07-27 10:13:47 +0500

    Addition of strict type method in param interface

--- src/Core/Request/ParamInterface.php
+++ src/Core/Request/ParamInterface.php
@@ -8,7 +8,14 @@ interface ParamInterface
 {
     public function required();
     public function serializeBy(callable $serializerMethod);
-    public function typeGroup(string $typeGroup, array $serializerMethods = []);
+    /**
+     * @param string   $strictType        Strict single type i.e. string, ModelName, etc. or group of types
+     *                                    in string format i.e. oneof(...), anyof(...)
+     * @param string[] $serializerMethods Methods required for the serialization of specific types in
+     *                                    in the provided strict types/type, should be an array in the format:
+     *                                    ['path/to/method argumentType', ...]. Default: []
+     */
+    public function strictType(string $strictType, array $serializerMethods = []);
     /**
      * @throws InvalidArgumentException
      */
