deb4f59ba96fda505c3c642ed8cecb16185476c3
Asad Ali <asad.ali@apimatic.io>
2022-07-28 09:26:38 +0500

    Updated request setter interface

--- src/Core/Request/RequestSetterInterface.php
+++ src/Core/Request/RequestSetterInterface.php
@@ -8,10 +8,9 @@ interface RequestSetterInterface extends RequestInterface
 {
     public function setHttpMethod(string $requestMethod): void;
     public function appendPath(string $path): void;
-    public function addQuery(string $key, $value, string $arrayFormat = RequestArraySerialization::INDEXED): void;
-    public function addTemplate(string $key, $value, bool $encode = true): void;
+    public function addTemplate(string $key, $value): void;
     public function addHeader(string $key, $value): void;
-    public function addFormParam(string $key, $value, string $arrayFormat = RequestArraySerialization::INDEXED): void;
+    public function addFormParam(string $key, $value, string $encodedBody): void;
     public function addBodyParam($value, ?string $key = null): void;
     public function setBodyAsXml(string $rootName): void;
     public function setBodyAsJson(): void;
