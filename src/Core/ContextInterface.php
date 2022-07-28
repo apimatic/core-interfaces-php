11bfe7078db62d90bf7af746399803c025dfd33b
Asad Ali <asad.ali@apimatic.io>
2022-07-28 11:53:42 +0500

    Fixed context interface

--- src/Core/ContextInterface.php
+++ src/Core/ContextInterface.php
@@ -4,11 +4,11 @@ namespace CoreDesign\Core;
 
 use CoreDesign\Core\Request\RequestInterface;
 use CoreDesign\Core\Response\ResponseInterface;
-use CoreDesign\Sdk\ConverterInterface;
 
-interface ContextInterface extends Convertable
+interface ContextInterface
 {
     public function getRequest(): RequestInterface;
     public function getResponse(): ResponseInterface;
-    public function convertIntoApiResponse($deserializedBody, ConverterInterface $converter);
+    public function convert();
+    public function convertIntoApiResponse($deserializedBody);
 }
--- src/Sdk/ConverterInterface.php
+++ src/Sdk/ConverterInterface.php
@@ -3,13 +3,12 @@
 namespace CoreDesign\Sdk;
 
 use CoreDesign\Core\ContextInterface;
-use CoreDesign\Core\CoreExceptionInterface;
 use CoreDesign\Core\Request\RequestInterface;
 use CoreDesign\Core\Response\ResponseInterface;
 
 interface ConverterInterface
 {
-    public function createApiException(string $message, RequestInterface $request, ?ResponseInterface $response);
+    public function createApiException(string $message, RequestInterface $request, ResponseInterface $response);
     public function createHttpContext(ContextInterface $context);
     public function createHttpRequest(RequestInterface $request);
     public function createHttpResponse(ResponseInterface $response);
